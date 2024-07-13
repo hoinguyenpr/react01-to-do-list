633c64b226311f5a7ba7e78b3bf39ae08c1cb6d3
hoinguyenpr <hoinguyenpr.97@gmail.com>
2024-07-13 13:51:45 +0700

    #30 JSX - fragment - css

--- src/components/learn/ComponentDefaultExport.jsx
+++ src/components/learn/ComponentDefaultExport.jsx
@@ -1,5 +1,14 @@
+import "./styles.css";
+
 function ComponentDefaultExport() {
-  return <div>My First Component!</div>;
+  return (
+    <>
+      <div>My First Component</div>
+      <div className="child" style={{ backgroundColor: "green" }}>
+        Child
+      </div>
+    </>
+  );
 }
 
 export default ComponentDefaultExport;
