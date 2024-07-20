f53217b18f3194b306bdaf8d4c67e36a9f78963c
hoinguyenpr <hoinguyenpr.97@gmail.com>
2024-07-20 11:33:56 +0700

    33 - Components exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import "./App.css";
 import ComponentDefaultExport from "./components/learn/ComponentDefaultExport";
 import {
   ComponentObjectExportA,
   ComponentObjectExportB,
 } from "./components/learn/ComponentObjectExport";
 import ComponentVariableInReact from "./components/learn/ComponentVariableInReact";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import reactLogo from "./assets/react.svg";
+import "./components/todo/todo.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,11 +21,14 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
-      <ComponentDefaultExport />
-      <ComponentObjectExportA />
-      <ComponentObjectExportB />
-      <ComponentVariableInReact />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoData />
+        <TodoNew />
+        <div className="todo-image">
+          <img src={reactLogo} className="logo" />
+        </div>
+      </div>
     </>
   );
 }
